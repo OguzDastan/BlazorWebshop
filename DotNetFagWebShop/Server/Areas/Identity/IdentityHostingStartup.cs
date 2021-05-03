dfa8ed4ee6a39a3229d6a0d53ce0da95308e2e10
Oguz Sefer Dastan <oguz0040@edu.easj.dk>
2021-05-04 01:07:16 +0200

    scaffolded identity server pages, changes some design

---
+++ DotNetFagWebShop/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DotNetFagWebShop.Server.Data;
+using DotNetFagWebShop.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DotNetFagWebShop.Server.Areas.Identity.IdentityHostingStartup))]
+namespace DotNetFagWebShop.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
